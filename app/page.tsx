f058178a9d7c19b73b488b06cc36f38f05d114ea
guilhermecooprativa <guilherme@cooprativa.pt>
2025-09-04 00:04:56 +0000

    Teste

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import React from 'react';
-import Header from '@/components/Header/Header';
-import Footer from '@/components/Footer/Footer';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 
 export default function page() {
   const scrollToSection = (sectionId: string) => {
