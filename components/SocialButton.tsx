2194f0faf7b75048683b2f2da21f5db23ec3ab5f
guilhermecooprativa <guilherme@cooprativa.pt>
2025-09-09 16:52:42 +0000

    teste

--- components/SocialButton.tsx
+++ components/SocialButton.tsx
@@ -1,5 +1,6 @@
-// @ts-nocheck
 "use client";
+
+import React from 'react';
 
 const SocialButton = () => {
   return (
@@ -17,13 +18,18 @@ const SocialButton = () => {
           viewBox="0,0,256,256" 
           width="24px" 
           height="24px" 
-          className="relative z-10 transition-colors duration-300 group-hover:fill-white"
+          className="relative z-20 transition-all duration-300"
           style={{fill: '#cc39a4'}}
         >
           <g transform="scale(8,8)">
             <path d="M11.46875,5c-3.55078,0 -6.46875,2.91406 -6.46875,6.46875v9.0625c0,3.55078 2.91406,6.46875 6.46875,6.46875h9.0625c3.55078,0 6.46875,-2.91406 6.46875,-6.46875v-9.0625c0,-3.55078 -2.91406,-6.46875 -6.46875,-6.46875zM11.46875,7h9.0625c2.47266,0 4.46875,1.99609 4.46875,4.46875v9.0625c0,2.47266 -1.99609,4.46875 -4.46875,4.46875h-9.0625c-2.47266,0 -4.46875,-1.99609 -4.46875,-4.46875v-9.0625c0,-2.47266 1.99609,-4.46875 4.46875,-4.46875zM21.90625,9.1875c-0.50391,0 -0.90625,0.40234 -0.90625,0.90625c0,0.50391 0.40234,0.90625 0.90625,0.90625c0.50391,0 0.90625,-0.40234 0.90625,-0.90625c0,-0.50391 -0.40234,-0.90625 -0.90625,-0.90625zM16,10c-3.30078,0 -6,2.69922 -6,6c0,3.30078 2.69922,6 6,6c3.30078,0 6,-2.69922 6,-6c0,-3.30078 -2.69922,-6 -6,-6zM16,12c2.22266,0 4,1.77734 4,4c0,2.22266 -1.77734,4 -4,4c-2.22266,0 -4,-1.77734 -4,-4c0,-2.22266 1.77734,-4 4,-4z" />
           </g>
         </svg>
+        <style jsx>{`
+          .group:hover svg {
+            fill: white !important;
+          }
+        `}</style>
       </a>
 
       {/* Facebook Button */}
@@ -35,11 +41,16 @@ const SocialButton = () => {
       >
         <div className="absolute inset-0 bg-blue-600 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
         <span 
-          className="relative z-10 text-2xl font-bold transition-colors duration-300 group-hover:text-white"
+          className="relative z-20 text-2xl font-bold transition-all duration-300"
           style={{color: '#1877F2'}}
         >
           f
         </span>
+        <style jsx>{`
+          .group:hover span {
+            color: white !important;
+          }
+        `}</style>
       </a>
     </div>
   );
